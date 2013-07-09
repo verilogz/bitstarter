293a32c23e1e69e993ba4fb8537f16cb04463e81
Ubuntu <ubuntu@ip-10-232-4-71.us-west-2.compute.internal>
2013-07-09 16:06:20 +0000

    buffer mistyped

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var bufer = new Buffer(30);
+var buffer = new Buffer(30);
 var fs = require('fs');
 var data = fs.readFileSync('./index.html',"utf8");
 buffer.write(data, "utf-8");  
