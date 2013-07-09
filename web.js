abc4f91b5c35b9c7a505c2f2c1241e7229d98942
Ubuntu <ubuntu@ip-10-232-4-71.us-west-2.compute.internal>
2013-07-09 16:14:01 +0000

    fixing bugs. display ok. buffer length changed

--- web.js
+++ web.js
@@ -3,17 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var buffer = new Buffer(30);
+var buffer = new Buffer(27);
 var fs = require('fs');
 var data = fs.readFileSync('./index.html',"utf8");
 buffer.write(data, "utf-8");  
-console.log(buffer);
-response.send(buffer);
- 
+response.send(buffer.toString()); 
 });
 
 var port = process.env.PORT || 5000;
-console.log("start");
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
