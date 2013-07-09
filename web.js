adca8d7ba53e52bb66745057ca1621e224aa6eeb
Ubuntu <ubuntu@ip-10-232-4-71.us-west-2.compute.internal>
2013-07-09 15:57:19 +0000

    webjs html file dir

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var bufer = new Buffer(30);
 var fs = require('fs');
-fs.readFileSync('/index.html', function(err,data)
+fs.readFileSync('./index.html', function(err,data)
 {
 if(err)throw err;
 buffer.write(data, "utf-8");  
